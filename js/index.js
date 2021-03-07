5523a62c18954b09bfdcfdce412f9e6fdf2b6d3a
Kriss Gardner <krissrobert12@gmail.com>
2021-03-08 00:43:37 +0200

    Remove blog dead code

--- js/index.js
+++ js/index.js
@@ -91,7 +91,6 @@ $(function () {
     gsap.to("#navigation-content", 0, { display: "none", delay: 0.7 });
     gsap.to("#navigation-content", 0, { y: "-100%", delay: 0.7 });
     gsap.to("#header", 0, { display: "none" });
-    gsap.to("#blog", 0, { display: "none" });
     gsap.to("#portfolio", 0, { display: "none" });
     gsap.to("#breaker", 0, { display: "block" });
     gsap.to("#breaker-two", 0, { display: "block", delay: 0.1 });
@@ -106,7 +105,6 @@ $(function () {
     gsap.to("#navigation-content", 0, { y: "-100%", delay: 0.7 });
     gsap.to("#header", 0, { display: "none" });
     gsap.to("#about", 0, { display: "none" });
-    gsap.to("#blog", 0, { display: "none" });
     gsap.to("#portfolio", 0, { display: "none" });
     gsap.to("#breaker", 0, { display: "block" });
     gsap.to("#breaker-two", 0, { display: "block", delay: 0.1 });
@@ -121,7 +119,6 @@ $(function () {
     gsap.to("#header", 0, { display: "none" });
     gsap.to("#about", 0, { display: "none" });
     gsap.to("#contact", 0, { display: "none" });
-    gsap.to("#blog", 0, { display: "none" });
     gsap.to("#breaker", 0, { display: "block" });
     gsap.to("#breaker-two", 0, { display: "block", delay: 0.1 });
     gsap.to("#breaker", 0, { display: "none", delay: 2 });
